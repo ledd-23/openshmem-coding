1ebbebbc7615504c1dfc10e37ed1e686387a4060
Danh Le <ledangdanh123@gmail.com>
2021-07-14 00:42:25 -0500

    added a makefile

--- exercises/broadcast.c
+++ exercises/broadcast.c
@@ -1,36 +1,67 @@
-#include <shmem.h>
-#include <stdio.h>
 #include <stdlib.h>
-int main(void) {
-	static long source[5], dest[5];
-	static long signal; //to hold signal from higher rank pe
-	static long value; //to hold value of assigned array element
+#include <stdio.h>
 
-	shmem_init();
+#include <shmem.h>
+#include <shmemx.h>
 
-	int mype = shmem_my_pe();
-	int npes = shmem_n_pes();
+#define MAXPROC 2112
 
-	if (mype == 0)
-		for (int i = 0; i < 5; i++)
-			source[i] = i;
-		
-	//maybe a put of some sort for each pe != 0 to hold a number in the array
-	shmem_broadcast(SHMEM_TEAM_WORLD, dest, source, 5, 0);
+int rank, size;
+uint64_t parent_sent, left_sent, right_sent; // signal flag values
+int64_t myval,leftval, rightval;             // local places to send/recv messages
 
-	//also a put here to put our value in the lower rank pe
-
-	if (mype != 5)
-		shmem_wait_until(signal, SHMEM_CMP_EQ, dest[mype]); //wait until signal is received from the higher rank pe
-	
-	long sum = signal + dest[mype - 1];
-
-	printf("current sum %d of pe %d", sum, mype);
-
-	shmem_barrier_all();
-
-	shmem_finalize();
-	return 0;
+void broadcast(int64_t *val) {
+	if (rank == 0){
+		for (int i = 0; i < size; i ++){
+			shmem_p(&myval, *val, i);
+		}
+	}	
 }
 
+
+
+void reduce(int64_t *local, int64_t *result) {
+	*result = *local*10;
+}
+
+
+
+int main(int argc, char **argv) {
+	  int64_t value = 0, reduceval = 0;
+	  setbuf(stdout, NULL);
+
+	  shmem_init();
+
+	  rank = shmem_my_pe();
+	  size = shmem_n_pes();
+
+	  if (rank == 0) {
+	       value = size;
+	  }
+
+          parent_sent = left_sent = right_sent = 0; //0 is unread, 1 is read
+          myval = leftval = rightval = 0;
+
+	  shmem_barrier_all();
+
+          broadcast(&value);
+
+          shmem_barrier_all();
+
+          for (int i=0; i<size; i++) {
+		if (i==rank)
+		   printf("rank %d received %ld\n", rank, myval);
+		   shmem_barrier_all();
+	  }
+
+	  shmem_barrier_all();
+
+          reduce(&value, &reduceval);
+
+          shmem_barrier_all();
+	  if (rank == 0)
+	      printf("reduced value is: %ld\n", reduceval);
+
+         shmem_finalize();
+}
 
